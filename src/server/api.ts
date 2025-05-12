8da2448da160984e2801671a7eab7bd06798955b
kamekuramaisuke <taisukeoono350@gmail.com>
2025-05-12 17:44:12 +0900

    workflowテスト

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,10 +1,7 @@
 import { remultApi } from 'remult/remult-sveltekit';
 import { User } from '../shared/User';
-import { Message } from '../shared/Message';
-import { Conversation } from '../shared/Conversation';
-import { ConversationMember } from '../shared/ConversationMember';
 
 export const api = remultApi({
-	entities: [User, Message, Conversation, ConversationMember],
+	entities: [User],
 	admin: true
 });
